2cc5012fd92ff55d0abc3d9c95d6de888c7f7beb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-08-30 15:18:24 +0900

    Update platforms.h

--- config/platforms.h
+++ config/platforms.h
@@ -9,7 +9,93 @@
 #include "platform_types.h"
 const struct hw_config supported_platforms[] = {
     {
+        .name = "DS916+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
         .name = "DS918+",
+        .pci_stubs = {
+            { .type = VPD_MARVELL_88SE9215,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_I211,          .bus = 0x02, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_I211,          .bus = 0x03, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_AHCI_CTRL, .bus = 0x00, .dev = 0x12, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_PCIE_PA,   .bus = 0x00, .dev = 0x13, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_PCIE_PB,   .bus = 0x00, .dev = 0x14, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_USB_XHCI,  .bus = 0x00, .dev = 0x15, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_I2C,       .bus = 0x00, .dev = 0x16, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_HSUART,    .bus = 0x00, .dev = 0x18, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x02, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x00, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x01, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x00, .multifunction = true },
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS1019+",
+        .pci_stubs = {
+            { .type = VPD_MARVELL_88SE9215,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_I211,          .bus = 0x02, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_I211,          .bus = 0x03, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_AHCI_CTRL, .bus = 0x00, .dev = 0x12, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_PCIE_PA,   .bus = 0x00, .dev = 0x13, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_PCIE_PB,   .bus = 0x00, .dev = 0x14, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_USB_XHCI,  .bus = 0x00, .dev = 0x15, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_I2C,       .bus = 0x00, .dev = 0x16, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_HSUART,    .bus = 0x00, .dev = 0x18, .fn = 0x00, .multifunction = false },
+            { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x02, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SPI,       .bus = 0x00, .dev = 0x19, .fn = 0x00, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x01, .multifunction = true },
+            { .type = VPD_INTEL_CPU_SMBUS,     .bus = 0x00, .dev = 0x1f, .fn = 0x00, .multifunction = true },
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS620slim",
         .pci_stubs = {
             { .type = VPD_MARVELL_88SE9215,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
             { .type = VPD_INTEL_I211,          .bus = 0x02, .dev = 0x00, .fn = 0x00, .multifunction = false },
@@ -67,7 +153,7 @@ const struct hw_config supported_platforms[] = {
         .pci_stubs = {
             { .type = __VPD_TERMINATOR__ }
         },
-        .emulate_rtc = false,
+        .emulate_rtc = true,
         .swap_serial = false,
         .reinit_ttyS0 = true,
         .fix_disk_led_ctrl = true,
@@ -77,8 +163,50 @@ const struct hw_config supported_platforms[] = {
             .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
             .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
                              HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
-            .sys_fan_speed_rpm = {HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
             .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS720+",
+        .pci_stubs = {
+            { .type = VPD_MARVELL_88SE9235,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS723+",
+        .pci_stubs = {
+            { .type = VPD_MARVELL_88SE9235,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
             .psu_status = { HWMON_PSU_NULL_ID },
             .sys_current = { HWMON_SYS_CURR_NULL_ID },
         }
@@ -119,7 +247,49 @@ const struct hw_config supported_platforms[] = {
             .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
             .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
                              HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
-            .sys_fan_speed_rpm = {HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS1821+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "DS1823xs+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
             .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
             .psu_status = { HWMON_PSU_NULL_ID },
             .sys_current = { HWMON_SYS_CURR_NULL_ID },
@@ -262,7 +432,28 @@ const struct hw_config supported_platforms[] = {
             .sys_current = { HWMON_SYS_CURR_NULL_ID },
         }
     },
-	{
+    {
+        .name = "DS1819+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
         .name = "DVA3219",
         .pci_stubs = {
             { .type = __VPD_TERMINATOR__ }
@@ -327,6 +518,49 @@ const struct hw_config supported_platforms[] = {
     },
     {
         .name = "FS6400",
+        .pci_stubs = {
+            { .type = VPD_INTEL_CPU_I2C,    .bus = 0x00, .dev = 0x16, .fn = 0x00, .multifunction = false },
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = true,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "RS1221+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "RS1619xs+",
         .pci_stubs = {
             { .type = __VPD_TERMINATOR__ }
         },
@@ -396,6 +630,27 @@ const struct hw_config supported_platforms[] = {
         }
     },
     {
+        .name = "RS3621xs+",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
         .name = "RS4021xs+",
         .pci_stubs = {
             { .type = __VPD_TERMINATOR__ }
@@ -403,7 +658,49 @@ const struct hw_config supported_platforms[] = {
         .emulate_rtc = false,
         .swap_serial = false,
         .reinit_ttyS0 = true,
-        .fix_disk_led_ctrl = false,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "SA3400",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .is_dt = false,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID,
+                             HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_NULL_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+    {
+        .name = "SA3600",
+        .pci_stubs = {
+            { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = false,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
         .has_cpu_temp = true,
         .is_dt = false,
         .hwmon = {
